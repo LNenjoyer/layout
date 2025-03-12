81e6c540b2404cb7d5e99480a411d266ecccaec5
LNenjoyer <ut_3779@zsp3zamosc.pl>
2025-03-12 08:41:38 +0100

    Dodaj pliki projektów.

---
+++ layout/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace layout
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ layout/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace layout
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+
+}
